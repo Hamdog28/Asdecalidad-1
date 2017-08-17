e8077fd50d648671d12a86163d21d456adcfa49c
María Laura <marilau63@gmail.com>
2017-08-17 09:03:58 -0600

    upload file

--- website/src/website/inicio/urls.py
+++ website/src/website/inicio/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^imagen',views.upload_file, name='upload_file'),
     url(r'^$',views.index, name='index'),
+    
 ]
--- website/src/website/inicio/views.py
+++ website/src/website/inicio/views.py
@@ -1,7 +1,27 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+import os 
 
 def index(request):
     template = loader.get_template('index.html')
     context={}
     return HttpResponse(template.render(context,request))
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        handle_uploaded_file(request.FILES['file'], str(request.FILES['file']))
+        return HttpResponseRedirect("/inicio")
+ 
+    return HttpResponse("Failed")
+ 
+def handle_uploaded_file(file, filename):
+    if not os.path.exists('upload/'):
+        os.mkdir('upload/')
+    
+    with open('upload/' + filename, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+          
