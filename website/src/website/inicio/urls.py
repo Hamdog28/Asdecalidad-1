59f9fffcbc5a174151564d455cfd233c8cd0b056
María Laura <marilau63@gmail.com>
2017-10-07 23:34:06 -0600

    Front end -> nuevo usuario

--- website/src/website/inicio/urls.py
+++ website/src/website/inicio/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^nuevo',views.nuevo_usuario, name='nuevo_usuario'),
     url(r'^imagen',views.upload_file, name='upload_file'),
+    url(r'^usuario',views.upload_files, name='upload_files'),
     url(r'^$',views.index, name='index'),
     
 ]
--- website/src/website/inicio/views.py
+++ website/src/website/inicio/views.py
@@ -11,10 +11,32 @@ def index(request):
     context={}
     return HttpResponse(template.render(context,request))
 
+def nuevo_usuario(request):
+    template = loader.get_template('nuevo_usuario.html')
+    context={}
+    return HttpResponse(template.render(context,request))
+
+def upload_files(request):
+    if request.method == 'POST':
+        handle_uploaded_file(request.FILES['file'], str(request.FILES['file']))
+        return HttpResponse('<script>function mensaje() {alert("Imagen ingresada con exito"); }mensaje();window.location.replace("http://127.0.0.1:8000/inicio");</script> ')
+        
+    return HttpResponse('<script>function mensaje() {alert("Imagen ingresada incorrectaente"); }mensaje();window.location.replace("http://127.0.0.1:8000/inicio");</script> ')
+
 
 def upload_file(request):
     if request.method == 'POST':
-        handle_uploaded_file(request.FILES['file'], str(request.FILES['file']))
+        Nombre = request.POST['nombre']
+        handle_uploaded_file(request.FILES['file1'], str(request.FILES['file1']))
+        handle_uploaded_file(request.FILES['file2'], str(request.FILES['file2']))
+        handle_uploaded_file(request.FILES['file3'], str(request.FILES['file3']))
+        handle_uploaded_file(request.FILES['file4'], str(request.FILES['file4']))
+        handle_uploaded_file(request.FILES['file5'], str(request.FILES['file5']))
+        handle_uploaded_file(request.FILES['file6'], str(request.FILES['file6']))
+        handle_uploaded_file(request.FILES['file7'], str(request.FILES['file7']))
+        handle_uploaded_file(request.FILES['file8'], str(request.FILES['file8']))
+        handle_uploaded_file(request.FILES['file9'], str(request.FILES['file9']))
+        handle_uploaded_file(request.FILES['file10'], str(request.FILES['file10']))
         return HttpResponse('<script>function mensaje() {alert("Imagen ingresada con exito"); }mensaje();window.location.replace("http://127.0.0.1:8000/inicio");</script> ')
         
     return HttpResponse('<script>function mensaje() {alert("Imagen ingresada incorrectaente"); }mensaje();window.location.replace("http://127.0.0.1:8000/inicio");</script> ')
